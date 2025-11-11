2be4ee83b84c0ec9e88feae6838e2d9a291da7d2
Colby E <colby.endres@tamu.edu>
2025-11-11 13:15:42 -0600

    Docstrings and Pylint

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import os 
+import os
 import logging
 from dotenv import load_dotenv
 
+
 class Config:
     load_dotenv()
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    DATABASE_URL = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
-    LOCAL_FILE_PATH = 'data/people.json'
+    DATABASE_URL = os.environ.get('DATABASE_URL').replace(
+        'postgres://', 'postgresql://')
     REMOTE_MATCH_FILE = 'data/match.csv'
-
-    
