21ae73b0f94abd80ccec06a4a39fd23d25970e86
Colby E <colby.endres@tamu.edu>
2026-02-23 11:33:29 -0600

    Unbreak test suite

--- src/mentormatch/config.py
+++ src/mentormatch/config.py
@@ -3,33 +3,34 @@ import logging
 from dotenv import load_dotenv
 
 
-class Logger:
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-    @staticmethod
-    def info(msg):
-        logging.info(msg)
-
-    @staticmethod
-    def warn(msg):
-        logging.warning(msg)
-
-    @staticmethod
-    def error(msg):
-        logging.error(msg)
-
-    @staticmethod
-    def debug(msg):
-        logging.debug(msg)
-
-
 class Config:
     load_dotenv()
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+    
+    @staticmethod
+    def parse_database_url(url):
+        modified_url = url 
+        if 'postgres://' in url:
+            # SQLAlchemy expects the URI to begin with postgresql
+            modified_url = url.replace('postgres://', 'postgresql://')
+            logging.debug('Using remote Postgres database')
+        elif 'sqlite:///' in url:
+            db_name = url.removeprefix('sqlite:///')
+            curr_file_path = os.path.abspath(__file__)
+            full_path = os.path.join(os.path.dirname(curr_file_path), 'data', db_name)
+            modified_url = f'sqlite:///{full_path}'
+        return modified_url
+    
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLAlchemy expects the URI to begin with postgresql
     DATABASE_URL = os.environ.get('DATABASE_URL')
-    if 'postgres://' in DATABASE_URL:
-        DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://')
+    TEST_DB_URL = os.environ.get('TEST_DB_URL')
+    
+    if DATABASE_URL is None:
+        raise ValueError('Set DATABASE_URL in file')
+    DATABASE_URL = parse_database_url(DATABASE_URL)
+    if TEST_DB_URL is not None:
+        print(f'{TEST_DB_URL=}')
+        TEST_DB_URL = parse_database_url(TEST_DB_URL)
+        
     REMOTE_MATCH_FILE = 'data/match.csv'
-    logger = Logger()
