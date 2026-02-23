892acc92deecb45ba2df2be1fe0d905e59dfd706
Colby E <colby.endres@tamu.edu>
2026-02-23 12:29:59 -0600

    Fix logger and removed Postgres order_by

--- src/mentormatch/matcher.py
+++ src/mentormatch/matcher.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import logging
 
 from mentormatch.models import Person, Preference
 from mentormatch.config import Config
@@ -49,7 +50,7 @@ class PeopleInfo:
                 self.db_index[(PeopleInfo.COL, self.num_mentees)] = person.id
                 self.num_mentees += 1
                 
-        Config.logger.info(f'Created indices with {self.num_mentors} mentors and {self.num_mentees} mentees')
+        logging.info(f'Created indices with {self.num_mentors} mentors and {self.num_mentees} mentees')
         self.indices_valid = True
 
     def construct_matrix(self):
@@ -63,7 +64,7 @@ class PeopleInfo:
         """
         # Cached matrix is in a valid state, can skip computation
         if self.matrix_valid:
-            Config.logger.debug('Returning cached pref matrix')
+            logging.debug('Returning cached pref matrix')
             return self.matrix
 
         # Indices are out-of-date, reconstitute them from prefs table
@@ -140,12 +141,12 @@ class PeopleInfo:
             self.matrix_index[p.id] = (PeopleInfo.ROW, self.num_mentors)
             self.db_index[(PeopleInfo.ROW, self.num_mentors)] = p.id
             self.num_mentors += 1
-            Config.logger.info(f'Added mentor {name} with id {p.id}')
+            logging.info(f'Added mentor {name} with id {p.id}')
         else:
             self.matrix_index[p.id] = (PeopleInfo.COL, self.num_mentees)
             self.db_index[(PeopleInfo.COL, self.num_mentees)] = p.id
             self.num_mentees += 1
-            Config.logger.info(f'Added mentee {name} with id {p.id}')
+            logging.info(f'Added mentee {name} with id {p.id}')
 
         self.matrix_valid = False
         self.matrix = None
@@ -176,7 +177,7 @@ class PeopleInfo:
         # NOTE: We cannot simply remove the person from the dictionary
         # This breaks the sequential nature of the values (i.e. the row/col indices)
         # NOTE: We could be more selective, keeping people whose status differs from the deleted person
-        Config.logger.debug(f'Deleted person {name} with id {person.id}, invalidating matrix')
+        logging.debug(f'Deleted person {name} with id {person.id}, invalidating matrix')
         self.matrix_index = {}
         self.db_index = {}
         self.matrix_valid = False
@@ -280,12 +281,17 @@ class PeopleInfo:
         db_ids = [self.db_index[(PeopleInfo.ROW, idx)] for idx in mentor_idx]
         db_ids.extend([self.db_index[(PeopleInfo.COL, idx)]
                       for idx in mentee_idx])
-        people = self.db.query(Person.name).filter(Person.id.in_(db_ids)).order_by(
-            func.array_position(db_ids, Person.id)).all()
-        people = [p for (p,) in people]
-        n = len(people) // 2
-        return people[:n], people[n:]
-
+        people = self.db.query(Person.id, Person.name).filter(Person.id.in_(db_ids)).all()
+        mentor_names = [None for _ in range(len(mentor_idx))]
+        mentee_names = [None for _ in range(len(mentee_idx))]
+        for db_id, name in people:
+            axis, idx = self.matrix_index[db_id]
+            if axis == PeopleInfo.ROW:
+                mentor_names[idx] = name
+            else:
+                mentee_names[idx] = name
+        return mentor_names, mentee_names
+    
     def get_from_name(self, user_name: str):
         """
         Retrieve person by name
@@ -322,7 +328,7 @@ class Matcher:
         """
         # Are we allowed to reuse the cached matching?
         if not force_rematch and self.matches and self.people_info.matrix_valid:
-            Config.logger.debug('Returning cached matching')
+            logging.debug('Returning cached matching')
             return self.matches
 
         mat = self.people_info.construct_matrix()
